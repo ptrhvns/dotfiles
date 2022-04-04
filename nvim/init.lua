f290cc39f8533f0b353da7e3829991bcae973f59
Peter Havens <peter.havens@gmail.com>
2022-04-04 11:08:09 -0600

    Remove commented out code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
--- vim.cmd [[
---     set runtimepath^=~/.vim runtimepath+=~/.vim/after
---     let &packpath=&runtimepath
---     source ~/.vimrc
--- ]]
-
 vim.g.mapleader = '\\'
 
 local opt = vim.opt

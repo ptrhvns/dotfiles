e3857e53a9bac42694cf778a2a7ee5f48a7be5c3
Peter Havens <peter.havens@gmail.com>
2022-04-08 11:30:35 -0600

    Remove Neovim files

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
-utils = require("user.utils")
-
-utils.prequire("impatient")
-utils.prequire("user.options")
-utils.prequire("user.plugins")
-utils.prequire("user.colors")
-utils.prequire("user.mappings")
-utils.prequire("user.autocmds")

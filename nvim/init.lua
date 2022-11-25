29116192f48f741554b1ad902fd9ea14dfb27e4b
Peter Havens <peter.havens@gmail.com>
2022-11-25 16:13:10 -0700

    treesitter still too buggy - disable highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -516,7 +516,8 @@ require('nvim-treesitter.configs').setup {
   sync_install = false,
 
   highlight = {
-    enable = true,
-    disable = { "lua" },
+    enable = false,
+    -- enable = true,
+    -- disable = { "lua" },
   },
 }

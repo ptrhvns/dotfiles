f74e314f801845f321d5eab2c67b400ba53b9eba
Peter Havens <peter.havens@gmail.com>
2023-02-13 07:09:51 -0700

    Improve lualine config

--- nvim/init.lua
+++ nvim/init.lua
@@ -458,7 +458,23 @@ require("fidget").setup()
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = "", right = ""},
+    section_separators = { left = "", right = ""},
+  },
+  sections = {
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      }
+    }
+  },
+  inactive_sections = {
+    lualine_c = {
+      {
+        "filename",
+        path = 1,
+      }
+    }
   },
 }

482c130bf675c0b323d2ecb411745489afafd60e
Peter Havens <peter.havens@gmail.com>
2023-03-04 13:29:58 -0700

    Use a dimmer theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -490,7 +490,7 @@ require('lualine').setup {
     component_separators = { left = "", right = ""},
     icons_enabled = false,
     section_separators = { left = "", right = ""},
-    theme = "dracula", -- TODO: change to "auto" when kanagawa works again
+    theme = "jellybeans", -- TODO: change to "auto" when kanagawa works again
   },
   sections = {
     lualine_c = {

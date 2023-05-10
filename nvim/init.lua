3d8ef3ddc23940951a06297ecad3dacaebe8e8b1
Peter Havens <peter.havens@gmail.com>
2023-05-10 14:07:03 -0600

    Remove Rust support

--- nvim/init.lua
+++ nvim/init.lua
@@ -137,10 +137,6 @@ autocmd("FileType", { group = augroup, pattern = "markdown", command = "setlocal
 autocmd("FileType", { group = augroup, pattern = "markdown", command = "setlocal tabstop=2" })
 autocmd("FileType", { group = augroup, pattern = "python", command = "setlocal softtabstop=4" })
 autocmd("FileType", { group = augroup, pattern = "python", command = "setlocal tabstop=4" })
-autocmd("FileType", { group = augroup, pattern = "rust", command = "setlocal noexpandtab" })
-autocmd("FileType", { group = augroup, pattern = "rust", command = "setlocal nolist" })
-autocmd("FileType", { group = augroup, pattern = "rust", command = "setlocal softtabstop=4" })
-autocmd("FileType", { group = augroup, pattern = "rust", command = "setlocal tabstop=4" })
 autocmd("FileType", { group = augroup, pattern = "sass", command = "EmmetInstall" })
 autocmd("FileType", { group = augroup, pattern = "scss", command = "EmmetInstall" })
 autocmd("FileType", { group = augroup, pattern = "scss", command = "setlocal iskeyword+=-" })
@@ -223,7 +219,6 @@ require('nvim-treesitter.configs').setup {
     "json",
     "lua",
     "python",
-    "rust",
     "scss",
     "typescript",
     "vim",
@@ -412,11 +407,6 @@ lspconfig.pyright.setup {
   on_attach = on_attach,
 }
 
-lspconfig.rust_analyzer.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-}
-
 lspconfig.tsserver.setup {
   capabilities = capabilities,
   on_attach = on_attach,
@@ -471,7 +461,6 @@ filetype("javascript", cmp_setup_config)
 filetype("json", cmp_setup_config)
 filetype("lua", cmp_setup_config)
 filetype("python", cmp_setup_config)
-filetype("rust", cmp_setup_config)
 filetype("scss", cmp_setup_config)
 filetype("sh", cmp_setup_config)
 filetype("toml", cmp_setup_config)

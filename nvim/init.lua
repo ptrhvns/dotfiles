229dae16caa45ff9a2222b7e3d7c1bb9488985cf
Peter Havens <peter.havens@gmail.com>
2025-02-05 08:41:35 -0700

    Use default laststatus

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,6 @@ vim.opt.foldmethod = "indent"
 vim.opt.ignorecase = true
 vim.opt.inccommand = "split"
 vim.opt.joinspaces = false
-vim.opt.laststatus = 3
 vim.opt.lazyredraw = true
 vim.opt.linebreak = true
 vim.opt.list = true
